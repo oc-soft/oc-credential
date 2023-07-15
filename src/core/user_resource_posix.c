9bd6559bc82a14c3e5b03799051515175984245f
Toshiyuki Ogawa <toshi@oc-soft.net>
2023-07-16 06:06:29 +0900

    I had source code for macos.

--- src/core/user_resource_posix.c
+++ src/core/user_resource_posix.c
@@ -2,6 +2,9 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <pwd.h>
 
 
---
+++ src/credential-helper/exe_info_darwin.c
@@ -0,0 +1,101 @@
+#include "exe_info.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <libproc.h>
+#include <string.h>
+
+/**
+ * allocate memory
+ */
+static void*
+exe_info_alloc(
+    size_t size);
+
+/**
+ * get executable name
+ */
+char*
+exe_info_get_exe_name()
+{
+    char* result;
+    char* path_buff;
+    path_buff = (char*)exe_info_alloc(PROC_PIDPATHINFO_SIZE);
+    result = NULL;
+    if (path_buff) {
+        int size;
+	size = proc_pidpath(getpid(), path_buff, PROC_PIDPATHINFO_SIZE);
+        if (size) {
+
+            char* name_ptr;
+            size_t i;
+            name_ptr = NULL;
+            for (i = size - 1; i > 0; i--) {
+                if (path_buff[i] == '/') {
+                    name_ptr = &path_buff[i + 1];
+                    break;
+                }
+            }
+            if (name_ptr) {
+                result = (char*)exe_info_alloc(size - i);
+                if (result) {
+                    memcpy(result, name_ptr, size - i - 1);
+                    result[size - i] = '\0';
+                }
+            }
+        }
+    }
+    if (path_buff) {
+        exe_info_free(path_buff);
+    }
+    return result;
+}
+
+/**
+ * get executable directory
+ */
+char*
+exe_info_get_exe_dir()
+{
+    char* result;
+    char* path_buff;
+    path_buff = (char*)exe_info_alloc(PROC_PIDPATHINFO_SIZE);
+    result = NULL;
+    if (path_buff) {
+        int size;
+	size = proc_pidpath((int)getpid(), path_buff, PROC_PIDPATHINFO_SIZE);
+        if (size) {
+
+            size_t i;
+            for (i = size - 1; i > 0; i--) {
+                if (result[i] == '/') {
+                    result[i] = '\0';
+                    break;
+                }
+            }
+        }
+    }
+    return result;
+}
+
+
+/**
+ * free resource
+ */
+void
+exe_info_free(
+    void* obj)
+{
+    free(obj);
+}
+
+/**
+ * allocate memory
+ */
+static void*
+exe_info_alloc(
+    size_t size)
+{
+    return malloc(size);
+}
+
+
